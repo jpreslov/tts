4d0060e212730849ef8b3f7048a4eeb2d687aa41
jpreslov <lovett_jp@outlook.com>
2021-10-04 11:40:27 -0500

    cannot play wav files

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import TextToSpeechV1
 from typing import Text
-import playsound
+import librosa
+from playsound import playsound
 
-url = 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/312320bb-a44c-4075-a694-8b3f6d0dfeb4'
-apikey = '6ok7WJNT8DoutGRBrfJyBjHgAuRIhkQiW9366VcyMznv'
-
-num = 0
-
-# def inc_num():
-#     global num
-#     num += 1
+url = "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/312320bb-a44c-4075-a694-8b3f6d0dfeb4"
+apikey = "6ok7WJNT8DoutGRBrfJyBjHgAuRIhkQiW9366VcyMznv"
 
 
-filename = './speech' + str(num) + '.mp3'
-filename2 = 'speech' + str(num) + '.mp3'
+def inc_num():
+    num = 0
+    num += 1
+    print(num)
+
+
+filename = "./speech.mp3"
+filename2 = "speech.mp3"
 authenticator = IAMAuthenticator(apikey)
 tts = TextToSpeechV1(authenticator=authenticator)
 tts.set_service_url(url)
 
-with open(filename, 'wb') as audio_file:
-    res = tts.synthesize("Thank you so much. This is what a hard work grindset will do. No days off,. Always working. Entrepeneuere",
+
+with open(filename, "wb") as audio_file:
+    res = tts.synthesize("Thank you soiouoiuoiuouoi much. Thoys oys whoyt a hoyrd woyrk groyndset will do. Noy doys oyff.",
                          accept="audio/mp3", voice="en-US_AllisonV3Voice").get_result()
     audio_file.write(res.content)
+    # y, sr = librosa.load(filename, sr=16000)
+    # y_shifted = librosa.effects.pitch_shift(y, sr, n_steps=4)
+    # audio_file.write(y_shifted)
 
-playsound.playsound(filename2)
+
+playsound(filename2)
